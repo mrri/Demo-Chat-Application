209eebe71280abf1a87ce56a3c8dcbcad2669038
Quoc Huy Ngo <quochuyngo.94@gmail.com>
2015-04-12 11:59:26 +0700

    ChatDemo
    Chat:
    + Chat giữa 2 client
    + Lưu message vào DB

---
+++ ChatDemo/src/main/java/com/mycompany/chatdemo/ConnectDB.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chatdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Quoc Huy Ngo
+ */
+public class ConnectDB {
+    private Connection connection;
+    String table = "message";
+    public ConnectDB() throws ClassNotFoundException {
+        try {
+            connect();
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    void connect() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://127.0.0.1:3306/chat";
+        connection = DriverManager.getConnection(url, "root", "");
+        System.err.println("Connect Success");
+    }
+    
+    public void InsertToDB(Message message) throws SQLException{
+        Statement statement = (Statement)connection.createStatement();
+        String query = "insert into " + table + " values('" + message.getMessage()
+                                                         + "','" + message.getFrom() + "','" + message.getTo() + "')";
+        statement.executeUpdate(query);
+    }
+            
+}
---
+++ ChatDemo/src/main/java/com/mycompany/chatdemo/Receiver.java
@@ -0,0 +1,62 @@
+
+package com.mycompany.chatdemo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Receiver extends Thread{
+     int port;
+    ServerSocket socketServer;
+    Socket socketReceiver;
+    //String message = "";
+    Message message;
+
+    public Receiver(int port){
+        this.port = port;
+    }
+    
+    public void connection() throws IOException, SQLException, ClassNotFoundException{
+        socketServer = new ServerSocket(port);
+        while(true){
+            socketReceiver = socketServer.accept();
+            /*BufferedReader bufferedMessage = new BufferedReader(new InputStreamReader(socketReceiver.getInputStream()));
+            message = bufferedMessage.readLine();
+            if(message.equals("EXIT"))
+                break;
+            else{
+                String Message = "Receiver: " + message;
+                //Storage into DB
+                ClientChat.connectDB.InsertToDB(message);
+                ClientChat.modelMessage.addElement(Message);
+            }*/
+            ObjectInputStream objectMessage = new ObjectInputStream(socketReceiver.getInputStream());
+            message = (Message) objectMessage.readObject();
+            ClientChat.to = message.getFrom();
+            ClientChat.connectDB.InsertToDB(message);
+            if(message.getMessage().equals("EXIT"))
+                break;
+            else
+                ClientChat.modelMessage.addElement(message.getFrom().toUpperCase() + " :" + message.getMessage());
+        }
+        socketServer.close();
+        System.out.println("Disconnect");
+    }
+    public void run(){
+         try {
+             connection();
+         } catch (IOException ex) {
+             Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (SQLException ex) {
+             Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+         } catch (ClassNotFoundException ex) {
+             Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    }
+}
